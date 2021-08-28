553566d9c55630e4221a007ef261a11a233787d7
JCasperGIT <jacqobin@gmail.com>
2021-08-28 05:07:33 -0600

    Finished styles

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -1,12 +1,20 @@
 var darkMode = function() {
 
-    var sheetLink = $("#darkmode-css")
+    var sheetLink = $("#darkmode-css");
+    var tooltipText = $("#darkmode-tooltip");
 
     if(sheetLink.attr("href")) {
+        console.log(tooltipText);
         sheetLink.removeAttr("href");
+        tooltipText.innerText = "Light";
+        tooltipText.innerHTML = "Light";
+        tooltipText.innerContent = "Light";
         localStorage.setItem("darkmode_portfolio", "false");
     } else {
         sheetLink.attr("href", "darkmode.css");
+        tooltipText.innerText = "Dark";
+        tooltipText.innerHTML = "Dark";
+        tooltipText.innerContent = "Dark";
         localStorage.setItem("darkmode_portfolio", "true");
     }
 }
